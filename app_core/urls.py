8320f83c1df54d1dae9042a6790d08e1e0aedd47
Pedro-Leles <pedro.alvim@soulasalle.com.br>
2025-05-26 17:59:40 -0300

    Começo da pagina de voluntarios

--- app_core/urls.py
+++ app_core/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('sobre/', views.sobre, name='sobre'),
     # path('contato/', views.contato, name='contato'),
-    path('volutario/', views.voluntario, name='voluntario'),
+    path('voluntario/', views.voluntario, name='voluntario'),
     path('projetos/', views.projeto, name='projeto'),
 ]
