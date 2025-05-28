c41b96c3dc986135eff6668f5fbc82b780b73a95
GABRIEL HIERRO AMORIM DOS SANTOS <gabrielhierro.amorim@hotmail.com>
2025-05-27 21:54:16 -0300

    Atualização Geral das Páginas

--- pestalozzi/settings.py
+++ pestalozzi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +123,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Aponta para a pasta 'static' do projeto
+    os.path.join(BASE_DIR, 'app_core', 'static'),
 ]
 
 # Default primary key field typevv
