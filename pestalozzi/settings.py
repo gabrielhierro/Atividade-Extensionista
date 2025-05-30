e7ba01141ecd15b3b8789fa9e9f7f738f00cf63c
GABRIEL HIERRO AMORIM DOS SANTOS <gabrielhierro.amorim@hotmail.com>
2025-05-30 16:06:19 -0300

    comentario

--- pestalozzi/settings.py
+++ pestalozzi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x+2-#ggrbef%ga=-(_@x2zm*dhb0ciu1j6m!4cznjtf%l_*100
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] # Allow all hosts for development; change in production
 
 
 # Application definition
