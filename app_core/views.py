2a7caeb8cbaba2a55df108e9818b4576fed24122
GABRIEL HIERRO AMORIM DOS SANTOS <gabrielhierro.amorim@hotmail.com>
2025-05-30 03:27:04 -0300

    Alterações nas páginas home, sobre, portal de comunicados e voluntario

--- app_core/views.py
+++ app_core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from app_portal.models import Comunicado
 
 def home(request):
     return render(request, 'home/index.html')
@@ -11,3 +12,7 @@ def voluntario(request):
 
 def projeto(request):
     return render(request, 'projetos/projetos.html')
+
+def home(request):
+    ultimo_comunicado = Comunicado.objects.order_by('-data_publicacao').first()
+    return render(request, 'home/index.html', {'ultimo_comunicado': ultimo_comunicado})
